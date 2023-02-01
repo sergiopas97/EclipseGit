960aff7cfa07c487607c04339a7c30f32cb482c4
Sergio <sergio041997@gmail.com>
2023-02-01 16:48:26 +0100

    nuestro resultado 2

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -2,5 +2,9 @@
 public class PruebaGit {
   public static void main[] args{
 	  System.out.println("este es el primer mensaje");
+	  int age;
+	  String edad;
+	  System.out.println("este es el segundo  mensaje");
+
   }
 }
